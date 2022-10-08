9ecb74c267e9c82f998bf760f1c74c57c071f8fa
Kedar Babaleshwar <babaleshwarkedar@gmail.com>
2022-10-08 17:42:48 +0530

    Initial Commit

--- Java/Algorithms/1-Sorting-Algorithms/3-Insertion-Sort/InsertionSort.java
+++ Java/Algorithms/1-Sorting-Algorithms/3-Insertion-Sort/InsertionSort.java
@@ -1,13 +1,33 @@
 import java.util.*;
 
 public class InsertionSort {
+
 	public static int[] insertionSort(int[] arr) {
+
+		for(int i = 1; i < arr.length; i++) {
+
+			//System.out.println(Arrays.toString(arr));
+			//System.out.println("----------------------");
+
+			int key = arr[i];
+			int prevIndex = i - 1;
+
+			while(prevIndex >= 0 && key < arr[prevIndex]) {
+				arr[prevIndex + 1] = arr[prevIndex];
+				--prevIndex;
+			}
+
+			arr[prevIndex + 1] = key;
+
+
+		}
+		return arr;
 	}
 
 	public static void main(String[] args) {
-		int[] arr = {10, 3, 6, 2, 4, 8};
+		int[] arr = {10, 3, 12, 2, 4, 8};
 
-		int[] sortedArray = inserionSort(arr);
+		int[] sortedArray = insertionSort(arr);
 
 		System.out.println(Arrays.toString(sortedArray));
 	}
